8194a363f84ad1c621921d9ffb5c6627bbcde525
Omega Terra Coriel <44127678+MissCoriel@users.noreply.github.com>
2021-08-10 20:33:20 -0500

    Add files via upload

---
+++ InvestmentMaximized/Investment/ConfigModel.cs
@@ -0,0 +1,14 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Investment
+{
+    class ConfigModel
+    {
+        public SButton stockMarket { get; set; } = SButton.K;
+    }
+}
